3ab9bde90a102fe2a506f8fad0ee4cd86806520c
Luan Rabelo <113604638+lrb7dev@users.noreply.github.com>
2022-10-10 17:53:36 -0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,8 +5,7 @@
    # Do not use this in production. Sample purpose only.
 
    # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <your GitHub username>
+   # Additional Authors: lrb7dev
 
    # Input:
    # p, principal amount
